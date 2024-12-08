a10d0c3239aa03f79ec2f86f028d5ff4e8095edc
Khushipandey-456 <kp9761560@gmail.com>
2024-12-08 10:01:11 +0530

    CheckEvenOdd

--- index.js
+++ index.js
@@ -3,9 +3,18 @@
 function checkprime(num){
     for(let i=2;i<=num-1;i++){
         if(num%i==0){ //num %i==0 not a prime num
-            return false
+            return false 
         }
     }// //num %i!==0  a prime num
     return true
 }
-console.log(checkprime(6))
+console.log("Is Number Prime  --> "+checkprime(7))
+
+// CheckEvenOdd 
+function evenOdd(num) {
+    if (num % 2 === 0) {
+        return true; // num is even
+    }
+    return false; // num is odd
+}
+console.log("Is Number Even  --> "+evenOdd(5))
